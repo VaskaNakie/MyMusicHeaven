7f3af1e8c8b3d7e3ddddc85f4c7e40dedbc1aa2c
Vaska Nakie <naqevaska@gmail.com>
2022-04-20 04:00:23 +0300

    fix to redirect uri

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 const clientId = '53787cadabfb471c8b588198803c9822';
 
-const redirectUri = 'https://https://vaskanakie.github.io/MyMusicHeaven/';
+const redirectUri = 'https://vaskanakie.github.io/MyMusicHeaven/';
 
 let accessToken; 
 
